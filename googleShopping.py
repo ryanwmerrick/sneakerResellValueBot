13af111b02fcbebb9fe2b856f6d4fb0c1d4f8082
ryanwmerrick <ryanmerrick86@gmail.com>
2025-08-14 14:55:08 -0400

    no headless mode

--- googleShopping.py
+++ googleShopping.py
@@ -17,7 +17,6 @@ def create_driver():
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_experimental_option('useAutomationExtension', False)
-    options.add_argument("--headless=new")
     options.add_argument("--window-size=1920,1080")
     options.add_argument(
         "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
